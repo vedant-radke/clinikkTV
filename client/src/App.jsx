ff417bf763cb38f1f136069dc2e2853abca488fe
vedant-radke <vradke1234@gmail.com>
2025-02-16 19:22:53 +0530

    resolve issues in responsiveness

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,12 @@ import UploadMedia from "./components/UploadMedia";
 
 function App() {
   return (
-    <div className="flex h-screen p-4 bg-gray-100">
+    <div className="flex flex-col md:flex-row h-screen p-4 bg-gray-100">
       <div className="flex-1 p-4 bg-white shadow-md rounded-lg overflow-auto">
         <MediaPlayer />
       </div>
 
-      <div className="w-1/4 p-4 bg-white shadow-lg rounded-lg ml-4">
+      <div className="w-full md:w-1/4 p-4 bg-white shadow-lg rounded-lg mt-4 md:mt-0 md:ml-4">
         <UploadMedia />
       </div>
     </div>
