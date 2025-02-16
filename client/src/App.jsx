c055cf2eb216e005856ed0edeffa46b787f8775d
vedant-radke <vradke1234@gmail.com>
2025-02-16 19:27:45 +0530

    fix height of video content

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,14 @@ import UploadMedia from "./components/UploadMedia";
 
 function App() {
   return (
-    <div className="flex flex-col md:flex-row h-screen p-4 bg-gray-100">
-      <div className="flex-1 p-4 bg-white shadow-md rounded-lg overflow-auto">
-        <MediaPlayer />
-      </div>
-
+    <div className="flex flex-col md:flex-row h-fit p-4 bg-gray-100">
       <div className="w-full md:w-1/4 p-4 bg-white shadow-lg rounded-lg mt-4 md:mt-0 md:ml-4">
         <UploadMedia />
       </div>
+      <div className="flex-1 p-4 bg-white min-h-screen h-fit shadow-md rounded-lg overflow-auto">
+        <MediaPlayer />
+      </div>
+
     </div>
   );
 }
